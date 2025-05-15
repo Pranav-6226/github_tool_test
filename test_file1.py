134a128c253270cc10ca32381dd2644730f64824
Pranav-6226 <ts-pranav.nair@rakuten.com>
2025-05-15 16:33:29 +0530

    Create test_file1.py

---
+++ test_file1.py
@@ -0,0 +1,11 @@
+ List[AnyHttpUrl] = [
+        "http://localhost",
+        "http://localhost:3000",
+        "https://localhost",
+        "https://localhost:3000",
+    ]
+
+ MONGODB_CLUSTER_URL: str = "localhost:27017"
+
+ MONGODB_USERNAME: str = ""
+ MONGODB_PASSWORD: str = ""
